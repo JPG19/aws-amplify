ab8432c27cbb96af796941a793dd3fc8d28335ae
JPG <jpgtv19@gmail.com>
2022-03-20 14:10:53 -0300

    changes for v2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is hosted in AWS Amplify
         </a>
       </header>
     </div>
